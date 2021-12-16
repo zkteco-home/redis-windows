34c624def0626bac9f44d9516cbec12a3e0e468b
zkteco-home <66519168+zkteco-home@users.noreply.github.com>
2021-12-16 18:51:53 +0530

    Update install_redis.cmd
    Fixed "install_redis.cmd" does failed when path has blank issue

--- install_redis.cmd
+++ install_redis.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-sc create redis binPath= "%CD%\redis-server.exe --service-run %CD%\redis.conf" DisplayName= "Redis" start= auto depend= TCPIP
+sc create redis binPath= "%CD%\redis-server.exe --service-run redis.conf" DisplayName= "Redis" start= auto depend= TCPIP
 sc description redis "Redis Cache Service"
 net start redis
 
