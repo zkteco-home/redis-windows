1ed303bf3f9b70544d061a55f456bcff4f0c34d8
michael <michael.wang@zkteco.in>
2022-08-24 18:39:34 +0530

    Fixed bug 28

--- install_redis.cmd
+++
@@ -1,7 +0,0 @@
-@echo on
-cd /d %~dp0
-sc create redis binPath= "\"%CD%\redis-server.exe\" --service-run \"%CD%\redis.conf\"" DisplayName= "Redis" start= auto depend= TCPIP
-sc description redis "Redis Cache Service"
-net start redis
-
-
